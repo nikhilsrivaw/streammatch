770ba51077a530ac9e5043fe180a114bd0d7e1d1
nikhilsrivaw <nikhilksrivastav190@gmail.com>
2025-08-29 21:22:46 +0530

    completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from "@/context/auth-context";
+import Navbar from "@/components/Navbar";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +26,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}
       >
-        {children}
+        <AuthProvider>
+          <div className="h-full flex flex-col">
+            <Navbar/>
+            {children}
+          </div>
+        
+        </AuthProvider>
       </body>
     </html>
   );
